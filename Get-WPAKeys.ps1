18e76f079f1e28d3e29302acc1dece4ea6a6a6ca
Mattias Borg <24293001+mattiasborg82@users.noreply.github.com>
2019-04-03 13:47:22 +0200

    Update Get-WPAKeys.ps1
    encodiings..

--- Get-WPAKeys.ps1
+++ Get-WPAKeys.ps1
@@ -3,7 +3,7 @@ function Get-WPAkey
 <#
 .Synopsis Get-WPAKey is a function to extract the WPA key for a specific SSID
 .DESCRIPTION Get the WPA key in clear text
-.EXAMPLE Get-WPAkey -SSID “MySSID”
+.EXAMPLE Get-WPAkey -SSID "MySSID"
 .EXAMPLE Get-WPAkey
 .NOTES Created by Mattias Borg 2015
  www.sec-labs.com
@@ -11,16 +11,16 @@ function Get-WPAkey
 param([Parameter(Mandatory=$false,
 ValueFromPipelineByPropertyName=$false,
 Position=0)]
-$SSID = (netsh wlan show profiles | Select-String “All User Profile”))
+$SSID = (netsh wlan show profiles | Select-String "All User Profile"))
 foreach ($profile in $SSID)
     {
             try{
-                $SSID = ($profile.ToString().Split(“:”)[1]).trim()
+                $SSID = ($profile.ToString().Split(":")[1]).trim()
                }catch{}
         try
         {
-            $Data = netsh wlan show profiles name=”$SSID” key=clear | select-String Authentication, Cipher, “Key Content”
-            $Data = $Data -split “:”
+            $Data = netsh wlan show profiles name="$SSID" key=clear | select-String Authentication, Cipher, "Key Content"
+            $Data = $Data -split ":"
             [string]$Authentication = $Data[1]
             [string]$Cipher = $Data[3]
             [string]$Key = $Data[9]
