79a79eefca58d5896be9c9b5114c75ea1af2b076
Mattias Borg <24293001+mattiasborg82@users.noreply.github.com>
2019-08-08 10:25:17 +0200

    Create Test-WDConnectivity.ps1
    WD connectivity analyzer

---
+++ PowerShell/Defender/Connectitivy/Test-WDConnectivity.ps1
@@ -0,0 +1,64 @@
+Function Test-WDURLS
+{
+$urls = (iwr "https://raw.githubusercontent.com/stefanschorling/SEC-LABS/master/PowerShell/Defender/Connectitivy/urls.txt").content
+
+foreach ($url in $urls)
+{
+
+	if ($url -like "http*")
+		{	
+			if ($url -like "*pki*")
+			{
+				$resp = iwr "https://www.microsoft.com/pkiops/docs/repository.htm"
+				$pkiUrls =  $resp.links.href
+				foreach ($pkiUrl in $pkiUrls)
+					{
+						if ($pkiUrl -like "*$url*")
+						{
+							$pkiresponse = iwr $pkiUrl
+							if ($pkiresponse.statuscode -eq "200")
+							{
+								$testresult = "Success"
+								break
+							}
+							else{$testresult = "false"}
+						}   
+					}
+			
+			}
+			else{
+			$response = iwr $url
+			if ($response.statuscode -eq "200")
+				{
+					
+					$testresult = "Success"
+
+				}
+				else{$testresult = "false"}
+				}
+		}
+		else
+			{
+			
+				
+				$result = test-netconnection -computername $url -port 443
+			
+				if ($result.TcpTestSucceeded)
+					{
+					
+						$testresult = "Success"
+					
+					}
+					else{$testresult = "false"}
+			}
+		
+	New-Object PSObject -Property @{
+						 URL = $url
+						 Success = $testresult
+						 
+						 }	
+	
+	
+
+}
+}
